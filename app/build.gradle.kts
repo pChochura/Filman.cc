c526fa52d569c5cb78ec4fed32efdf5cc5c0ce76
Paweł Chochura <pchochura@gmail.com>
2026-08-05 13:24:32 +0200

    versionCode bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.pointlessapps.filman"
         minSdk = 24
         targetSdk = 37
-        versionCode = 8
-        versionName = "1.2"
+        versionCode = 9
+        versionName = "1.3"
     }
 
     buildTypes {
